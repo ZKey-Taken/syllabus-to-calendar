09e3f4200bdeec1363c236b6826f3aa63b087abb
Z_Key <83915288+ZKey-Taken@users.noreply.github.com>
2025-09-03 22:30:17 -0400

    Simple frontend upload file, backend incomplete.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, type ChangeEvent } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const API_URL = 'api/convert'; // TODO: Create backend endpoint, if using Vercel create serverless /api endpoint
+  const [file, setFile] = useState<File | null>(null);
+  const [isConverting, setIsConverting] = useState<boolean>(true)
 
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const uploadedFile = e?.target?.files?.[0] || null;
+    setFile(uploadedFile);
+  }
+
+  const handleUploadClick = async () => {
+    if (!file) { // Ensures file exists, fallback
+      alert("Please upload a file!");
+      return;
+    }
+
+    setIsConverting(true);
+    const formData = new FormData()
+    formData.append('file', file);
+
+    try {
+      const res = await fetch(API_URL, {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (res.ok) {
+        const result = await res.json();
+
+        console.log("Result:", result);
+        alert("Success!");
+      } else {
+        alert("Something went wrong!");
+      }
+    } catch (error) {
+      alert("Error: " + error);
+    }
+
+    setIsConverting(false)
+  }
+
+  // Only allowing pdf uploads
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>Syllabus to Calendar</h1>
+      <div id='inputDiv'>
+        <input type='file' accept='.pdf' onChange={handleFileChange} />
+        <button onClick={handleUploadClick} disabled={!file}>Submit</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <h3>{isConverting && "Please wait, converting PDF to calendar ..."}</h3>
+    </div>
   )
 }
 
